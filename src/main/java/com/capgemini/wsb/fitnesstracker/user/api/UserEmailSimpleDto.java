cebb51a7b41163ff559acca3c68cabf522b7ad48
Dariusz76145 <wrx71565@student.wsb.wroclaw.pl>
2024-11-23 15:49:26 +0100

    Lab 3

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/api/UserEmailSimpleDto.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/api/UserEmailSimpleDto.java
@@ -2,4 +2,7 @@ package com.capgemini.wsb.fitnesstracker.user.api;
 
 import jakarta.annotation.Nullable;
 
-record UserSimpleDto(@Nullable Long id, String firstName, String lastName){}
+/**
+ * UserSimpleDto is a simple representation of User entity.
+ */
+public record UserEmailSimpleDto(@Nullable Long id, String email){}
--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserEmailSimpleMapper.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserEmailSimpleMapper.java
@@ -1,4 +1,3 @@
-
 package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
@@ -6,10 +5,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 class UserEmailSimpleMapper {
+    /**
+     * Maps User entity to UserEmailSimpleDto
+     * @param user User
+     * @return UserEmailSimpleDto
+     */
     UserEmailSimpleDto toEmailSimpleDto(User user) {
         return new UserEmailSimpleDto(user.getId(), user.getEmail());
     }
 
+    /**
+     * Maps UserEmailSimpleDto to User entity
+     * @param userDto UserEmailSimpleDto
+     * @return User
+     */
     User toSimpleEmailEntity(UserEmailSimpleDto userDto) {
         return new User(null, null, null, userDto.email());
     }
